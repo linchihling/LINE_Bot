65562f6ee1d7e66110f3d35b622725878376256e
I309 <winnie916@tunghosteel.com>
2024-08-26 10:30:58 +0800

    移動程式位置

--- linebot_logic/member_status.py
+++
@@ -1,7 +0,0 @@
-def get_member_status(members, logger, user_id):
-    if user_id in members:
-        # logger.info(f"{members[user_id]} logged in")
-        return True
-    else:
-        logger.info(f"{user_id} is not a member")
-        return False
